9dd593572df4251fca0969120f39485c20671494
Paul Parsons <paulparsons@gmail.com>
2013-09-17 15:35:51 -0400

    preliminary code to allow user to select individual tree map cells

--- public/js/modules/grants.js
+++ public/js/modules/grants.js
@@ -2545,8 +2545,28 @@ var GRANTS = (function () {
             .data(children)
             .enter().append("g")
             .attr("class", "cell child")
+            .on("mouseover", function(d) {
+              if (this.children[0].style.stroke!="#ff0000"){
+                this.children[0].style.stroke="#ffffff"
+                this.children[0].style.strokeWidth="3px";
+              }
+            })
+            .on("mouseout", function() {
+              if (this.children[0].style.stroke=="#ffffff")
+                this.children[0].style.strokeWidth="0px";
+            })
             .on("click", function(d) {
-               zoom(node === d.parent ? root : d.parent);
+              // if (treemapselect)
+              //   console.log("selected");
+              if(this.children[0].style.stroke=="" || this.children[0].style.stroke=="#ffffff"){
+                this.children[0].style.stroke="#ff0000";
+                this.children[0].style.strokeWidth="3px";
+              }
+              else if(this.children[0].style.stroke=="#ff0000"){
+                this.children[0].style.stroke="#ffffff";
+                this.children[0].style.strokeWidth="0px";
+              }
+              //zoom(node === d.parent ? root : d.parent);
             });
     childCells.append("rect")
             .style("fill", function(d) {
