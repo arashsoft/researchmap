891d4e6c901a7b74aae709b15445e9c82dd2207c
Paul Parsons <paulparsons@gmail.com>
2014-03-07 13:12:09 -0500

    added some error handling to the app

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-//test line to make Arman happy
-//Arman is testing
-//Arman is testing through SmartGit
-//Arman is testing through SmartGit again
+'use strict';
+
 var express = require('express');
+var errorHandler = require('express-error-handler');
+var logger = require('bunyan-request-logger');
 var http = require('http');
-var app = express();
 var exphbs = require( 'express3-handlebars' );
+var noCache = require('connect-cache-control');
+
+var app = express();
+var log = logger();
+var port = process.env.myapp_port || 3000;
+
+// Create the server object that we can pass
+// in to the error handler:
+var server = http.createServer(app);
 
 //requirements for the routes
 var collaborations = require('./routes/collaborations');
@@ -25,11 +33,43 @@ app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.engine('handlebars', exphbs({defaultLayout: 'main' }));
     app.set('view engine', 'handlebars');
+
+    //use the given middleware functions
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
 	app.use(express.static(__dirname + "/public"));
+	app.use(log.requestLogger());
+	app.use(log.errorLogger());
+
+	// Respond to errors and conditionally shut
+	// down the server. Pass in the server object
+	// so the error handler can shut it down
+	// gracefully:
+	app.use( errorHandler({server: server}) );
 });
+
+
+// Route to handle client side log messages.
+//
+// Counter to intuition, client side logging
+// works best with GET requests.
+// 
+// AJAX POST sends headers and body in two steps,
+// which slows it down.
+// 
+// This route prepends the cache-control
+// middleware so that the browser always logs
+// to the server instead of fetching a useless
+// OK message from its cache.
+app.get('/log', noCache,
+    function logHandler(req, res) {
+
+  // Since all requests are automatically logged,
+  // all you need to do is send the response:
+  res.send(200);
+});
+
 
 //routes for pages
 app.get("/collaborations", collaborations.page);
@@ -39,6 +79,16 @@ app.get("/views", views.page);
 app.get("/industry", industry.page);
 //route for default home
 app.get("/", home.page);
+
+
+// Route that triggers a sample error:
+app.get('/error', function createError(req,
+    res, next) {
+  var err = new Error('Sample error');
+  err.status = 500;
+  next(err); //synonymous with res.sedn(status, error)
+});
+
 
 //routes for processing data
 app.get("/processData", processData.full);
@@ -50,9 +100,16 @@ app.get("/matrix/:x", matrix.data);
 app.get("/grants/:x", grants.data);
 
 
+// Route that triggers a sample error:
+//app.all('/*', errorHandler.httpError(404));
+
 
 //server
-app.listen( 3000, function(){
-	console.log( 'server running on port 3000...' );
+// app.listen( 3000, function(){
+// 	console.log( 'server running on port 3000...' );
+// });
+
+server.listen(port, function () {
+  log.info('Listening on port ' + port);
 });
 
